04653a4d2bfcc37f2d2221ea23ed2867f4c4ceb4
BrittleFoot <buftioo@gmail.com>
2024-06-05 15:19:01 +0400

    annotation example

--- src/books/models.py
+++ src/books/models.py
@@ -1,11 +1,13 @@
 from app.models import TimestampedModel
 from django.db import models
+from django.db.models.manager import Manager
 
 
 class Author(TimestampedModel):
     name = models.CharField(max_length=255)
+    books: "Manager[Book]"
 
 
 class Book(TimestampedModel):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
     name = models.CharField(max_length=255)
